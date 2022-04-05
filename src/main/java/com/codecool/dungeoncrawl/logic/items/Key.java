1aa68688606af9eb22db6140bd2a13420c74dd54
DianaDob <dobsinszki.diana@gmail.com>
2022-04-05 09:43:23 +0200

    feat: add classes Item (abstract), Key (extends Item) and Sword (extends Item)

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Key.java
@@ -0,0 +1,8 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+public class Key extends Item{
+
+    public String getTileName() {
+        return "key";
+    }
+}
---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Sword.java
@@ -0,0 +1,8 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+public class Sword extends Item{
+
+    public String getTileName() {
+        return "sword";
+    }
+}
