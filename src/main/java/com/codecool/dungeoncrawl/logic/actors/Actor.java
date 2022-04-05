7a4467e398575a0b187270aab821b1d200f71a10
DianaDob <dobsinszki.diana@gmail.com>
2022-04-05 14:02:16 +0200

    feat: add open and closed doors and player can move through door

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -4,20 +4,30 @@ import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Actor implements Drawable {
     private Cell cell;
     private int health = 10;
 
+
     public Actor(Cell cell) {
         this.cell = cell;
         this.cell.setActor(this);
+
     }
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (nextCell.getType() == CellType.WALL || nextCell.getType()== CellType.EMPTY || nextCell.getType()== CellType.SKELETON){
+        if (nextCell.getType() == CellType.WALL || nextCell.getType() == CellType.EMPTY || nextCell.getType() == CellType.SKELETON){
+
             nextCell = cell;
             cell.setActor(this);
+        }
+        if(nextCell.getType() == CellType.CLOSED_DOOR &&
+                (cell.getActor() instanceof Player && cell.getActor().getInventory().containsKey("key"))){
+            nextCell.setType(CellType.OPEN_DOOR);
         }
         cell.setActor(null);
         nextCell.setActor(this);
@@ -27,6 +37,10 @@ public abstract class Actor implements Drawable {
 
     public int getHealth() {
         return health;
+    }
+
+    public Map<String, Integer> getInventory() {
+        return cell.getActor().getInventory();
     }
 
     public Cell getCell() {
