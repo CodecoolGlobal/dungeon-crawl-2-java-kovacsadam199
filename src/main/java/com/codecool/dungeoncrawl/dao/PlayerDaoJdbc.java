11f324cc2092c00543b04c22f80109c32972d214
kovacsadam199 <kovacsadam199@gmail.com>
2022-04-20 10:46:54 +0200

    feat: getALl players and get player added

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
@@ -4,6 +4,7 @@ import com.codecool.dungeoncrawl.model.PlayerModel;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerDaoJdbc implements PlayerDao {
@@ -34,15 +35,42 @@ public class PlayerDaoJdbc implements PlayerDao {
     @Override
     public void update(PlayerModel player) {
 
+
     }
 
     @Override
     public PlayerModel get(int id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, player_name, hp, x, y FROM player WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1,id);
+            statement.execute();
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+                PlayerModel player = new PlayerModel(rs.getString(2), rs.getInt(4), rs.getInt(5));
+                player.setHp(rs.getInt(3));
+                player.setId(rs.getInt(1));
+            return player;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all authors", e);
+        }
     }
 
     @Override
     public List<PlayerModel> getAll() {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, player_name, hp, x, y FROM player";
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            List<PlayerModel> result = new ArrayList<>();
+            while (rs.next()) {
+                PlayerModel player = new PlayerModel(rs.getString(2), rs.getInt(4),rs.getInt(5));
+                player.setHp(rs.getInt(3));
+                player.setId(rs.getInt(1));
+                result.add(player);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all authors", e);
+        }
     }
 }
